dedfcb05383d679e7ee5b877249110281a373dd4
Sadeep Madurange <smadurange@users.noreply.github.com>
2020-04-10 15:37:49 +0000

    Successfully connect to host.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#include <boost/system/error_code.hpp>
 #include <iostream>
+#include <ostream>
 #include <string>
 #include <sstream>
 
@@ -13,16 +15,24 @@ int main(int argc, char *argv[])
     const std::string port = "993";
     
     boost::asio::io_service ioService;
-
+    boost::asio::ip::tcp::socket socket(ioService);
     boost::asio::ip::tcp::resolver resolver(ioService);
     boost::asio::ip::tcp::resolver::query query(hostname, port);
     boost::asio::ip::tcp::resolver::iterator iter = resolver.resolve(query);
     boost::asio::ip::tcp::resolver::iterator end;
-
+    
     while(iter != end)
     {
         boost::asio::ip::tcp::endpoint ep = *iter++;
-        std::cout << ep << std::endl;
+        std::cout << "Trying to connect to " << ep << std::endl;
+        boost::system::error_code connErr;
+        socket.connect(ep, connErr);
+        
+        if(!connErr)
+        {
+            std::cout << "Connected to " << ep << std::endl;
+            break;
+        };
     }
 
     return 0;
