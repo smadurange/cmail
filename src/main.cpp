297da3483b7b414d6a7b745c0837eb4ba9b27991
Sadeep Madurange <smadurange@users.noreply.github.com>
2020-04-11 07:47:11 +0000

    Read response from server working!

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <boost/asio/buffer.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/read.hpp>
+#include <boost/asio/read_until.hpp>
+#include <boost/asio/streambuf.hpp>
 #include <boost/asio/write.hpp>
 #include <iostream>
+#include <istream>
 #include <string>
 #include <sstream>
 
@@ -22,7 +25,7 @@ namespace ssl = boost::asio::ssl;
 int main(int argc, char *argv[])
 {
     const std::string hostname = "outlook.office365.com";
-    const std::string port = "993";
+    const std::string port = "143";
     
     boost::asio::io_service ioService;
 
@@ -42,6 +45,32 @@ int main(int argc, char *argv[])
     }
 
     std::cout << "Connected to IMAP server: " << connectedEndpoint << std::endl;
+    
+    boost::system::error_code writeError;
+    size_t wbytes = boost::asio::write(socket, boost::asio::buffer("A001 CAPABILITY\r\n"), writeError);
+
+    if(writeError)
+    {
+        std::cout << "Failed to send command: " << writeError.message() << std::endl;
+        return -1;
+    }
+
+    std::cout << "Sent " << wbytes << " bytes to server." << std::endl;
+
+    boost::system::error_code readError;
+    boost::asio::streambuf sb;
+    boost::asio::read_until(socket, sb, '\n', readError);
+
+    if (readError)
+    {
+        std::cout << "Failed to get response from server: " << readError << std::endl;
+        return -1;
+    }
+    
+    std::istream is(&sb);
+    std::string line;
+    std::getline(is, line);
+    std::cout << "Server responded: " << line  << std::endl;
 
     return 0;
 }
