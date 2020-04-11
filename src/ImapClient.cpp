7f4e7ae2c76d2919a1a47fb53545c49f282ea582
Sadeep Madurange <smadurange@users.noreply.github.com>
2020-04-11 19:12:05 +0000

    Code formatting.

--- src/ImapClient.cpp
+++ src/ImapClient.cpp
@@ -1,4 +1,3 @@
-#include <boost/asio/ssl/verify_mode.hpp>
 #include <istream>
 #include <stdexcept>
 #include <string>
@@ -10,6 +9,7 @@
 #include <boost/asio/streambuf.hpp>
 #include <boost/asio/ssl/context.hpp>
 #include <boost/asio/ssl/stream.hpp>
+#include <boost/asio/ssl/verify_mode.hpp>
 
 #include <spdlog/spdlog.h>
 
@@ -27,28 +27,16 @@ void cindel::ImapClient::connect(const std::string &hostname, const std::string 
 {
     boost::system::error_code error;
     ip::tcp::resolver::iterator endpoints = resolver.resolve(hostname, port, error);
-    
-    if (error)
-    {
-        throw std::runtime_error("Failed to resolve hostname: " + error.message());
-    }
+    if (error) throw std::runtime_error("Failed to resolve hostname: " + error.message());
 
-    ip::tcp::resolver::iterator iter = boost::asio::connect(socket.lowest_layer(), endpoints, error);
-    
-    if(error)
-    {
-        throw std::runtime_error("Failed to connect to IMAP server: " + error.message());
-    }
-    
-    ip::tcp::endpoint endpoint = *iter;
+    ip::tcp::resolver::iterator it = boost::asio::connect(socket.lowest_layer(), endpoints, error);
+    if(error) throw std::runtime_error("Failed to connect to IMAP server: " + error.message());
+    ip::tcp::endpoint endpoint = *it;
     spdlog::debug("Connected to IMAP server: " + endpoint.address().to_string());
+
     socket.set_verify_mode(ssl::verify_none);
     socket.handshake(ssl::stream<ip::tcp::socket>::client, error);
-
-    if (error)
-    {
-        throw std::runtime_error("SSL handshake failed: " + error.message());
-    }
+    if (error) throw std::runtime_error("SSL handshake failed: " + error.message());
 
     boost::asio::streambuf buf;
     boost::asio::read_until(socket, buf, '\n', error);
@@ -56,6 +44,5 @@ void cindel::ImapClient::connect(const std::string &hostname, const std::string 
     std::string response;
     std::getline(is, response);
     buf.consume(buf.size());
-
     spdlog::trace(response);
 }
