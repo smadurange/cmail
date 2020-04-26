cb58d244f1b99549c01045b9617c81b84ff68a56
Sadeep Madurange <smadurange@users.noreply.github.com>
2020-04-26 12:44:46 +0000

    Use rapid json for configuration.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include <list>
+#include <rapidjson/encodings.h>
 #include <string>
 
 #include <mailio/imap.hpp>
 
 #include <ncurses.h>
 
+#include <rapidjson/document.h>
+#include <rapidjson/filereadstream.h>
+
 #include <spdlog/common.h>
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
+rapidjson::Document initConfig()
+{
+    std::FILE *fp = std::fopen("config.json", "r");
+    char buf[65536];
+    rapidjson::FileReadStream fs(fp, buf, sizeof(buf));
+    rapidjson::Document doc;
+    doc.ParseStream(fs);
+    fclose(fp);
+    return doc;
+}
+
 int main(int argc, char *argv[])
 {
-    const std::string host = "outlook.office365.com";
-    int port = 993;
-    const std::string username = "test.20200410@outlook.com";
-    const std::string password = "MN3SbbTVYviMi55F";
+    rapidjson::Document config = initConfig();
 
     auto console = spdlog::stdout_color_mt("console");  
     spdlog::set_default_logger(console);
@@ -25,6 +37,11 @@ int main(int argc, char *argv[])
     raw();
     noecho();
     keypad(stdscr, true);
+    
+    std::string host = config["host"].GetString();
+    int port = config["port"].GetInt();
+    std::string username = config["username"].GetString();
+    std::string password = config["password"].GetString();
 
     mailio::imaps conn(host, port);
     conn.authenticate(username, password, mailio::imaps::auth_method_t::LOGIN);
