57d4f556d233b311ac327cfa0d1739329371c40d
Sadeep Madurange <smadurange@users.noreply.github.com>
2020-04-26 18:49:47 +0000

    Implemented open connection.

--- src/imap/Connection.cpp
+++ src/imap/Connection.cpp
@@ -1,12 +1,19 @@
+#include <mutex>
+#include <openssl/asn1.h>
+#include <string>
+
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ssl/context.hpp>
 #include <boost/system/error_code.hpp>
+
+#include <spdlog/spdlog.h>
+#include <sys/socket.h>
 
 #include "Connection.hpp"
 #include "Response.hpp"
 
 cmail::Connection::Connection(boost::asio::io_context &ctx, boost::asio::ssl::context &ssl)
-    : socket(ctx, ssl)
+    : resolver(ctx), socket(ctx, ssl)
 {
 }
 
@@ -18,11 +25,49 @@ cmail::Connection &cmail::Connection::instance()
     return conn;
 }
 
-void cmail::Connection::open(const std::string &host, int port)
+cmail::Response cmail::Connection::open(const std::string &host, int port)
 {
-    mtx.lock();
+    std::lock_guard<std::mutex> lock(mtx);
+    cmail::Response response;
     if(connected)
-        return;
+        return response("", "", true);
+
+    boost::system::error_code error;
+    boost::asio::ip::tcp::resolver::query query(host, std::to_string(port));
+    spdlog::trace("Attempting to resolve endpoint.");
+    boost::asio::ip::tcp::resolver::iterator endpoints = resolver.resolve(query, error);
+    if(error)
+    {
+        std::string err = "Failed to resolve hostname: " + error.message();
+        spdlog::error(err);
+        return response("", err, false);
+    }
     
-    mtx.unlock()
+    spdlog::trace("Resolved endpoints for " + host);
+    spdlog::trace("Sending connection request to server.");
+    boost::asio::ip::tcp::resolver::iterator it = boost::asio::connect(socket.lowest_layer(), endpoints, error);
+    if(error)
+    {
+        std::string err = "Failed to connect to IMAP server: " + error.message();
+        spdlog::error(err);
+        return response("", err, false);
+    }
+
+    socket.set_verify_mode(ssl::verify_none);
+    spdlog::warn("SSL certificate validation is set to verify_none.");
+    spdlog::trace("Initiating SSL handshake.");
+    socket.handshake(ssl::stream<ip::tcp::socket>::client, error);
+    if(error)
+    {
+        std::string err = "SSL handshake failed: " + error.message();
+        spdlog::error(err);
+        socket.shutdown();
+        socket.close();
+        return response("", err, false);
+    }
+    
+    spdlog::trace("SSL handshake completed.");
+    connected = true;
+    spdlog::info("Connected to IMAP server: " + it->address().to_string());
+    return 
 }
--- src/imap/Connection.hpp
+++ src/imap/Connection.hpp
@@ -19,14 +19,15 @@ namespace cmail
         Connection(Connection const&) = delete;
         Connection &operator=(Connection const&) = delete;
         ~Connection() = default;
-        void open(const std::string &host, int port);
-        void close();
+        Response open(const std::string &host, int port);
+        Response close();
         Response send(const std::string &request);
 
     private:
         bool connected{false};
         std::mutex mtx;
         std::atomic<int> counter{0};
+        boost::asio::ip::tcp::resolver resolver;
         boost::asio::ssl::stream<boost::asio::ip::tcp::socket> socket;
 
         Connection(boost::asio::io_context &ctx, boost::asio::ssl::context &ssl);
