eb0c318b09a324d3738f8ec1e6e509737c639416
Sadeep Madurange <16175248+smadurange@users.noreply.github.com>
2020-04-26 11:21:24 +0800

    Added ncurses initialisation. (#14)
    Co-authored-by: Sadeep Madurange <smadurange@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,36 @@
 
 #include <mailio/imap.hpp>
 
+#include <ncurses.h>
+
 #include <spdlog/common.h>
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
 int main(int argc, char *argv[])
 {
-    auto console = spdlog::stdout_color_mt("console");  
-    spdlog::set_default_logger(console);
-    spdlog::set_level(spdlog::level::trace);
-
     const std::string host = "outlook.office365.com";
     int port = 993;
     const std::string username = "test.20200410@outlook.com";
     const std::string password = "MN3SbbTVYviMi55F";
 
+    auto console = spdlog::stdout_color_mt("console");  
+    spdlog::set_default_logger(console);
+    spdlog::set_level(spdlog::level::trace);
+
+    initscr();
+    raw();
+    noecho();
+    keypad(stdscr, true);
+
     mailio::imaps conn(host, port);
     conn.authenticate(username, password, mailio::imaps::auth_method_t::LOGIN);
 
     mailio::imaps::mailbox_stat_t stat = conn.statistics("inbox");
-    std::cout << "Number of messages in mailbox: " << stat.messages_no << std::endl;
+    printw("Number of messages in mailbox: %d",stat.messages_no);
+
+    getch();
+    endwin();
 
     return 0;
 }
